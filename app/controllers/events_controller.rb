873c46a1215ddaa798afbf880191965d1004f996
shanqyeet <shanqyeet@gmail.com>
2018-01-18 19:08:59 +0800

    created event show page skeleton

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,21 @@
 class EventsController < ApplicationController
+
+  def show
+    @event = Event.find(params[:id])
+    @user = User.find(params[:user_id])
+  end
+
+  def new
+  end
+
+  def created
+
+  end
+
+
+  private
+
+  def event_params
+    params.require(:event).permit()
+  end
 end
