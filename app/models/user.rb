51aa45dca81c1ce9b399a2d9739af673f57fc2cd
Ryan <junheng102@gmail.com>
2018-01-18 14:14:49 +0800

    add fb login method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,21 @@ class User < ApplicationRecord
 
   enum verification: { Unverified: 0, Verified: 1 }
   enum role: { user: 0, moderator: 1, superadmin: 2 }
+
+  def self.create_with_auth_and_hash(authentication, auth_hash)
+     user = self.create!(
+      name: auth_hash["extra"]["raw_info"]["name"],
+       email: auth_hash["extra"]["raw_info"]["email"],
+       remote_avatar_url: auth_hash["info"]["image"],
+       password: SecureRandom.hex(16)
+     )
+     user.authentications << authentication
+     return user
+  end
+
+   # grab fb_token to access Facebook for user data
+   def fb_token
+     x = self.authentications.find_by(provider: 'facebook')
+     return x.token unless x.nil?
+   end
 end
