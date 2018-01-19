7d3afce2d11a688b841cee43892fc81878e55532
shanqyeet <shanqyeet@gmail.com>
2018-01-19 15:44:30 +0800

    created testimonials controller

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
       only: [:create, :edit, :update]
     resources :events
     resources :messages
+    resources :testimonials, only:[:show, :create, :edit, :update]
   end
 
   resources  :organizatons do
