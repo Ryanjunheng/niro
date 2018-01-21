83ae31ea1b4c8dedc2c944cd55c6d9965c853856
shanqyeet <shanqyeet@gmail.com>
2018-01-21 15:51:40 +0800

    completed all organization redirecting function

--- app/controllers/event_messages_controller.rb
+++ app/controllers/event_messages_controller.rb
@@ -1,5 +1,6 @@
 class EventMessagesController < ApplicationController
 
+# user method
   def create
     @event_message = EventMessage.new(event_message_params)
     if @event_message.save
@@ -10,9 +11,6 @@ class EventMessagesController < ApplicationController
     end
   end
 
-  def show
-  end
-
   def update
     @event_message = EventMessage.find_by(user_id:current_user.id, event_id:params[:event_id], id:params[:id])
     @event_message.update(event_message_params)
@@ -20,6 +18,29 @@ class EventMessagesController < ApplicationController
   end
 
   def destroy
+    @event_message = EventMessage.find_by(user_id:current_user.id, event_id:params[:event_id], id:params[:id])
+    @event_message.destroy
+    redirect_to request.referrer
+  end
+
+# organization method
+  def org_create
+    @event_message = EventMessage.new(event_message_params)
+    if @event_message.save
+      redirect_to request.referrer
+    else
+      redirect_to '/'
+      @flash = {error: "there was a problem creting your event message, please try again"}
+    end
+  end
+
+  def org_update
+    @event_message = EventMessage.find_by(user_id:current_user.id, event_id:params[:event_id], id:params[:id])
+    @event_message.update(event_message_params)
+    redirect_to request.referrer
+  end
+
+  def org_destroy
     @event_message = EventMessage.find_by(user_id:current_user.id, event_id:params[:event_id], id:params[:id])
     @event_message.destroy
     redirect_to request.referrer
--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,5 +1,6 @@
 class ParticipationsController < ApplicationController
 
+# User event participation methods
   def create
     @participation = Participation.new(participation_params)
     if @participation.save
@@ -8,9 +9,6 @@ class ParticipationsController < ApplicationController
       redirect_to user_event_path(user_id: @participation.user_id, id: @participation.event_id)
       @flash = {error: "There was a problem submitting your missiong request, please try again"}
     end
-  end
-
-  def show
   end
 
   def complete
@@ -28,7 +26,36 @@ class ParticipationsController < ApplicationController
   def destroy
     @participation = Participation.find_by(user_id: current_user.id, event_id: params[:event_id])
     @participation.destroy
-    redirect_to user_path(current_user.id)
+    redirect_to request.referrer
+  end
+
+# Organization participation methods
+  def org_create
+    @participation = Participation.new(participation_params)
+    if @participation.save
+      redirect_to user_path(current_user.id)
+    else
+      redirect_to request.referrer
+      @flash = {error: "There was a problem submitting your missiong request, please try again"}
+    end
+  end
+
+  def complete
+    @participant = Participation.find(params[:id])
+    @participant.Completed!
+    redirect_to '/'
+  end
+
+  def fail
+    @participant = Participation.find(params[:id])
+    @participant.Failed!
+    redirect_to '/'
+  end
+
+  def org_destroy
+    @participation = Participation.find_by(user_id: current_user.id, event_id: params[:event_id])
+    @participation.destroy
+    redirect_to request.referrer
   end
 
   private
