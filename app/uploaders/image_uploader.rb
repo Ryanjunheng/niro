802c3832646d876016bad101e83e0a8ebf1fb61d
Kaiwee <kaiweepuah0121@hotmail.com>
2018-01-24 00:58:54 +0800

    modified all pages which have image or photos

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -22,7 +22,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fit: [80, 80]
+  process resize_to_fit: [1000, 376]
   #
   # def scale(width, height)
   #   # do something
