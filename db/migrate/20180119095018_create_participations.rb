7e8024cd259e974e0e7f968164fd41564e7ced3f
Ryan <junheng102@gmail.com>
2018-01-23 21:57:07 +0800

    add the line at user show page to show badges

---
+++ db/migrate/20180119095018_create_participations.rb
@@ -0,0 +1,9 @@
+class CreateParticipations < ActiveRecord::Migration[5.1]
+  def change
+    create_table :participations do |t|
+      t.references :user, foreign_key: true, on_delete: :cascade, null: true
+      t.references :event, foreign_key: true, on_delete: :cascade, null: true
+      t.timestamps
+    end
+  end
+end
